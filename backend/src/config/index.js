080782b11e818790d7c4a3b7c5fb17d89b76cd7f
enayetsyl <enayetflweb@gmail.com>
2024-10-31 00:43:20 +0600

    config file name corrected

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -4,11 +4,11 @@ dotenv.config();
 
 export default {
   port: process.env.PORT,
-  database_url: process.env.DATABASE_URL,
+  database_url: process.env.database_url,
   NODE_ENV: process.env.NODE_ENV,
-  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
-  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
-  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  cloudinary_cloud_name: process.env.cloudinary_cloud_name,
+  cloudinary_api_key: process.env.cloudinary_api_key,
+  cloudinary_api_secret: process.env.cloudinary_api_secret,
+  bcrypt_salt_rounds: process.env.bcrypt_salt_rounds,
   jwt_secret: process.env.JWT_SECRET
 };
