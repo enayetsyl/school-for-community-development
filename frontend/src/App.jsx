fe523dd9360b71497fd1dd703c7ef23a7cca0615
enayetsyl <enayetflweb@gmail.com>
2024-11-03 20:57:27 +0600

    post page and register page import changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from 'react-hot-toast';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-
-import RegisterPage from './pages/RegisterPage';
-import CreatePostPage from './pages/CreatePostPage';
-import PostPage from './pages/PostPage';
-import LoginPage from './pages/LoginPage';
-import PrivateRoute from './components/PrivateRoute';
-import UserManagementPage from './pages/UserManagementPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CreatePostPage from "./pages/CreatePostPage";
+import LoginPage from "./pages/LoginPage";
+import PrivateRoute from "./components/PrivateRoute";
+import UserManagementPage from "./pages/UserManagementPage";
+import RegisterPage from "./pages/RegisterPage";
+import PostPage from "./pages/PostPage";
 
 const queryClient = new QueryClient();
 
@@ -24,23 +23,23 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
-              <Route 
-    path="/create-post" 
-    element={
-      <PrivateRoute>
-        <CreatePostPage />
-      </PrivateRoute>
-    } 
-  />
+              <Route
+                path="/create-post"
+                element={
+                  <PrivateRoute>
+                    <CreatePostPage />
+                  </PrivateRoute>
+                }
+              />
               <Route path="/post/:id" element={<PostPage />} />
-              <Route 
-  path="/user-management" 
-  element={
-    <PrivateRoute>
-      <UserManagementPage />
-    </PrivateRoute>
-  } 
-/>
+              <Route
+                path="/user-management"
+                element={
+                  <PrivateRoute>
+                    <UserManagementPage />
+                  </PrivateRoute>
+                }
+              />
             </Routes>
           </main>
           <Toaster />
